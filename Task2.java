4e192d83a3911655a1d5c2c8478c9ddf6ec1051f
Maksim Lanin <maksimlanin@mail.ru>
2023-03-26 23:14:23 +0300

    Семинар 1 задание 2 ЛанинМС

---
+++ Task2.java
@@ -0,0 +1,15 @@
+package Java_LaninMS.Seminar1;
+
+/**
+ * Task2
+ */
+public class Task2 {
+
+    public static void main(String[] args) {
+        System.out.printf("Числа от  1 до 1000 \n");
+        for( int i = 1; i <= 1000; i++){
+            
+            System.out.printf("%d, ",i);
+        }
+    }
+}
